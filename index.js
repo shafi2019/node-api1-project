67de459c6776e1689adbd71b60b267e3a977edf5
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-18 01:04:30 -0800

    delete and post requests done

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 server.listen(5000, () => {
     console.log('=== server listening on port 5000 ===');
 });
@@ -23,7 +25,7 @@ server.get('/api/users', (req, res) => {
      })
 })
 
-server.get('./api/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     db.findById(req.params.id)
      .then(user => {
          if (!user) {
@@ -33,14 +35,56 @@ server.get('./api/users/:id', (req, res) => {
          }
      })
      .catch(error => {
-         res.status(500).json({ errorMessage: "The user information could not be retrieved.", error})
+         res.status(500).json({ errorMessage: "The user information could not be retrieved.", error});
      });
 });
-/*
-server.post('/api/users', (req, res) => {
-  const userInfo = req.body;
 
-  db.userInfo((users) =
-    .then
-})
-*/
+/*
+server.post("/api/users", (req, res) => {
+    const { name, bio } = req.body;
+    db.insert(req.body)
+      .then(data => {
+        if (!name || !bio) {
+          res.status(400).json({
+            errorMessage: "Please provide name and bio for the user." });
+        } else {
+          res.status(201).json(data);
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          errorMessage: "There was an error while saving the user to the database" });
+      });
+  });
+  */
+
+server.post('/api/users',  (req, res) => {
+    const userInfo = req.body;
+
+    db.insert(userInfo)
+      .then(user => {
+          if (req.body.name === "" || req.body.bio === "") {
+              res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+          } else{
+              res.status(201).json(user)
+          }
+      })
+      .catch(error => {
+          res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+      });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    db.remove(req.body.id) 
+      .then(user => {
+          if(!user) {
+              res.status(404).json({ message: "The user with the specified ID does not exist." })
+          } else{
+              res.status(201).json(user);
+          }
+      })
+      .catch(error => {
+          res.status(500).json({ errorMessage: "The user could not be removed" })
+      });
+});
+
