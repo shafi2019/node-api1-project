fd8ecea5db7f15201c53d8e913bd548ec878ab43
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-18 01:18:36 -0800

    update added

--- index.js
+++ index.js
@@ -88,3 +88,39 @@ server.delete('/api/users/:id', (req, res) => {
       });
 });
 
+
+server.put('/api/users/:id', (req, res) => {
+    db.update(req.params.id, req.body)
+    .then(user => {
+        if (!user){
+            res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+        } else if ((req.body.name.length<1) || (req.body.bio.length<1)){
+            res.status(400).json({errorMessage:  "Please provide name and bio for the user." })
+        } else {
+           res.status(200).json(user); 
+        }
+    })
+    .catch(error => {
+        res.status(500).json({errorMessage:  "The user information could not be modified."});
+    });
+});
+
+
+/*
+server.put("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = req.body;
+  
+    db.update(id, user)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json({ success: true, updated });
+        } else {
+          res.status(404).json({ success: false, message: "id not found" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ success: false, err });
+      });
+  });
+*/
